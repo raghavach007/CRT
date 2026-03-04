7b44176f1bcc194e6e0eba5fe3e34d5c16215fd0
raghavach007 <raghavachakravarthula07@gmail.com>
2026-03-04 09:28:19 +0530

    Assignments Submission

--- IVth - sem/Python/assignments/M01_Language_and_Programming/AST01/task.py
+++ IVth - sem/Python/assignments/M01_Language_and_Programming/AST01/task.py
@@ -1,12 +1,14 @@
+
+
 def Ticket_Pricing(n: int) -> int:
-   if n < 5:
-      return "Free"
-   elif n >= 5 and n<=17:
-      return "$10"
-   elif n>=18 and n<=64:
-      return "$20"
-   else:
-      return"$15"
+    if n < 5:
+        return 0
+    elif 5 <= n <= 17:
+        return 10
+    elif 18 <= n <= 64:
+        return 20
+    else:  # age >= 65
+        return 15
 
 
 if __name__ == '__main__':
